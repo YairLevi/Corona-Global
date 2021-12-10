d54b7c2126740675098671f092b910ea5926920b
Yair Levi <yairlevi2001@gmail.com>
2021-12-10 11:58:29 +0200

    Revert "Can add dates beyond max and min dates"
    This reverts commit b5e481b3a00a7d2f104d7d80139fd32b953b0f55.

--- js/table.js
+++ js/table.js
@@ -67,7 +67,9 @@ async function updateValue() {
 
     if (!is_valid_datalist_value('countries', country)) {
         text = 'Invalid Country'
-    } else if (date === '') {
+    } else if (new Date(date) > new Date(endDates['last_date'])
+        || new Date(date) < new Date(endDates['first_date'])
+        || date === '') {
         text = 'Invalid Date'
 
     } else if (!is_valid_datalist_value('dynamic-variables', variable)) {
